867533621680edb0d85f7ee17d0ce3949ff5cb94
bloghubmedia <pipheroes@gmail.com>
2026-06-07 14:02:37 +0700

    Create lang-map.js

---
+++ pipheroes/id/lang-map.js
@@ -0,0 +1,27 @@
+window.langMap = {
+  id: {
+    "/": {
+      en: "https://en.pipheroes.com/",
+      my: "https://my.pipheroes.com/"
+    },
+
+    "/2025/04/apa-itu-rebate.html": {
+      en: "https://en.pipheroes.com/2025/04/what-is-rebate.html",
+      my: "https://my.pipheroes.com/2025/04/apa-itu-rebat.html"
+    }
+  },
+
+  en: {
+    "/": {
+      id: "https://id.pipheroes.com/",
+      my: "https://my.pipheroes.com/"
+    }
+  },
+
+  my: {
+    "/": {
+      id: "https://id.pipheroes.com/",
+      en: "https://en.pipheroes.com/"
+    }
+  }
+};
