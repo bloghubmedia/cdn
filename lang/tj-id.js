d7a74310995594f10cb9ad2d195e559e44dae66c
bloghubmedia <pipheroes@gmail.com>
2026-06-07 09:41:59 +0700

    Create tj-id.js

---
+++ lang/tj-id.js
@@ -0,0 +1,69 @@
+window.langMap = {
+
+  /* =========================
+     INDONESIA SITE
+  ========================== */
+  id: {
+
+    "/": {
+      en: "https://en.pipheroes.com/",
+      my: "https://my.pipheroes.com/"
+    },
+
+    "/2025/04/apa-itu-rebate.html": {
+      en: "https://en.pipheroes.com/2025/04/what-is-rebate.html",
+      my: "https://my.pipheroes.com/2025/04/apa-itu-rebat.html"
+    },
+
+    "/2025/04/apa-itu-trading-forex.html": {
+      en: "https://en.pipheroes.com/2025/04/what-is-forex-trading.html",
+      my: "https://my.pipheroes.com/2025/04/apa-itu-trading-forex.html"
+    }
+
+  },
+
+  /* =========================
+     ENGLISH SITE
+  ========================== */
+  en: {
+
+    "/": {
+      id: "https://id.pipheroes.com/",
+      my: "https://my.pipheroes.com/"
+    },
+
+    "/2025/04/what-is-rebate.html": {
+      id: "https://id.pipheroes.com/2025/04/apa-itu-rebate.html",
+      my: "https://my.pipheroes.com/2025/04/apa-itu-rebat.html"
+    },
+
+    "/2025/04/what-is-forex-trading.html": {
+      id: "https://id.pipheroes.com/2025/04/apa-itu-trading-forex.html",
+      my: "https://my.pipheroes.com/2025/04/apa-itu-trading-forex.html"
+    }
+
+  },
+
+  /* =========================
+     MALAY SITE
+  ========================== */
+  my: {
+
+    "/": {
+      id: "https://id.pipheroes.com/",
+      en: "https://en.pipheroes.com/"
+    },
+
+    "/2025/04/apa-itu-rebat.html": {
+      id: "https://id.pipheroes.com/2025/04/apa-itu-rebate.html",
+      en: "https://en.pipheroes.com/2025/04/what-is-rebate.html"
+    },
+
+    "/2025/04/apa-itu-trading-forex.html": {
+      id: "https://id.pipheroes.com/2025/04/apa-itu-trading-forex.html",
+      en: "https://en.pipheroes.com/2025/04/what-is-forex-trading.html"
+    }
+
+  }
+
+};
